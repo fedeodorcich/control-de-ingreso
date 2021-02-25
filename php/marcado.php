b3653fdc0a544d1e2c92cee6828c672490827fef
fedeodorcich <32882548+fedeodorcich@users.noreply.github.com>
2021-02-25 13:34:08 -0300

    newdoc

---
+++ php/marcado.php
@@ -0,0 +1,6 @@
+<?php 
+	require('conexion.php');
+	//-------verificar si la ultima salida es igual que la entrada
+	//-------cargar salida o cargar entrada segun corresponda
+	//-------modificar la cuestion de las entradas y salidas
+ ?>
