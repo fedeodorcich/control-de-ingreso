dd80f50ecf0c13aeac0b629233d4d62f20d81d9c
fedeodorcich <32882548+fedeodorcich@users.noreply.github.com>
2021-02-25 09:11:31 -0300

    changes

--- php/getPeople.php
+++ php/getPeople.php
@@ -1,10 +1,9 @@
 <?php 
 	require('conexion.php');
 	$val=$_POST['value'];
-	$consulta='SELECT * FROM nombres WHERE value="$val"';
+	$consulta="SELECT * FROM nombres WHERE value='$val'";
 	$query=mysqli_query($conexion,$consulta);
 	while($res=mysqli_fetch_array($query)){
-		
 		echo "<option value='".$res['nombre']."'>".$res['nombre']."</option>";
 	}
  ?>
