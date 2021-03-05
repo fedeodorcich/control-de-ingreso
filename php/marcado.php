dc06acf4aa2b82f188f670c66dc14998cd91c4cf
Poyuyo <poyuyo2013@gmail.com>
2021-03-05 13:16:42 -0300

    Update marcado.php

--- php/marcado.php
+++ php/marcado.php
@@ -11,19 +11,29 @@
 		$preprequery=mysqli_query($conexion,"SELECT * FROM asistencia WHERE nombre='$nombre' ORDER BY entrada DESC LIMIT 1");
 		$consult=mysqli_fetch_array($preprequery);
 
-		if($consult['entrada']==$consult['salida']){
-					
-			$id=$consult['id'];
-			$entrada=$consult['entrada'];
-					
-			$salida = "UPDATE `asistencia` SET salida = CURRENT_TIMESTAMP , entrada = '$entrada' WHERE id='$id' ";
-			$query = mysqli_query($conexion,$salida);
-			if($query){
-				echo 'Marcado de salida correcto';
-			}else{
-				echo 'ERROR AL MARCAR SALIDA</div>';
-			}
+		if($consult){
+			if($consult['entrada']==$consult['salida']){
+						
+				$id=$consult['id'];
+				$entrada=$consult['entrada'];
+						
+				$salida = "UPDATE `asistencia` SET salida = CURRENT_TIMESTAMP , entrada = '$entrada' WHERE id='$id' ";
+				$query = mysqli_query($conexion,$salida);
+				if($query){
+					echo 'Marcado de salida correcto';
+				}else{
+					echo 'ERROR AL MARCAR SALIDA</div>';
+				}
 
+			}else{
+				$entrada="INSERT INTO `asistencia` (`id`, `nombre`, `entrada`,`salida`) VALUES ('0', '$nombre', current_timestamp(),current_timestamp())";
+				$query=mysqli_query($conexion,$entrada);
+				if($query){
+					echo 'Marcado entrada correcto';			
+				}else{
+					echo 'ERROR AL MARCAR ENTRADA';
+				}
+			}
 		}else{
 			$entrada="INSERT INTO `asistencia` (`id`, `nombre`, `entrada`,`salida`) VALUES ('0', '$nombre', current_timestamp(),current_timestamp())";
 			$query=mysqli_query($conexion,$entrada);
@@ -33,7 +43,6 @@
 				echo 'ERROR AL MARCAR ENTRADA';
 			}
 		}
-
 	}else{
 		echo false;
 	}
